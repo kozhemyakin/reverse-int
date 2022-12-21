b9222facc8222a09db7a76012bb96a20999e4519
kozhemyakin <pctdoc@gmail.com>
2022-12-21 16:31:21 +0300

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const positiveNumber = Math.abs(n);
+    const numberToString = positiveNumber.toString();
+    const reverseString = +numberToString.split('').reverse().join('');
+    const stringToNumber = +reverseString;
+
+    return stringToNumber;
 }
